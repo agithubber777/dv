6be3ec917b168264d2cdfaa3fd875d0a1e68e18d
Agit Hubber <agit.hubber@gmail.com>
2023-07-01 21:38:59 +0200

    + Coverage collector

---
+++ uvm/uvm_tb_template/src/env/tb_coverage_collector.sv
@@ -0,0 +1,12 @@
+//==============================================================================
+// Copyright (C) 2023 agithubber777
+//------------------------------------------------------------------------------
+// File        : tb_coverage_collector.sv
+// Description : TB Coverage Collector
+// Author      : agithubber777 (agit.hubber@gmail.com)
+// Created     : 2023/05/17
+//==============================================================================
+`ifndef TB_COVERAGE_COLLECTOR_SV
+    `define TB_COVERAGE_COLLECTOR_SV
+
+`endif // TB_COVERAGE_COLLECTOR_SV
